54fa9ff5d9520aeaab1a22eeba414c7cd9663747
Martin Finnerup <contact@codeslasher.com>
2021-03-18 00:27:46 +0100

    Started adding UI for creating events.

--- AwesomeSoft.Meet/ClientApp/src/components/WeeklyCalendar.js
+++ AwesomeSoft.Meet/ClientApp/src/components/WeeklyCalendar.js
@@ -1,10 +1,33 @@
 ﻿import React, { Component } from 'react';
 import moment from 'moment';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input, Row, Col } from 'reactstrap';
+import DatePicker from 'react-datepicker';
 import './CalendarCommon.css';
 import './WeeklyCalendar.css';
 
 export class WeeklyCalendar extends Component {
   static displayName = WeeklyCalendar.name;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false,
+            newEvent: {
+                title: "",
+                description: "",
+                startTime: moment().toISOString(),
+                endTime: moment().toISOString(),
+            }
+        };
+
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState(prevState => ({
+            modal: !prevState.modal
+        }));
+    }
 
     render() {
         const days = [];
@@ -57,7 +80,7 @@ export class WeeklyCalendar extends Component {
         return (
         <div>
             <div className="calendar_container">
-                <div className="title">{this.props.children} { moment().format("MMMM YYYY [Week] w") }</div>
+                <div className="title"><div className="side_options"><Button onClick={this.toggle}>New event</Button> {this.props.children}</div> { moment().format("MMMM YYYY [Week] w") }</div>
                 <div className="days">
                         <div className="filler"></div>
                         <div className="filler"></div>
@@ -76,6 +99,35 @@ export class WeeklyCalendar extends Component {
                     </div>
                 </div>
             </div>
+          <Modal isOpen={this.state.modal} toggle={this.toggle}>
+            <ModalHeader toggle={this.toggle}>Add event</ModalHeader>
+            <ModalBody>
+              <Form>
+                <FormGroup>
+                    <Label for="title">Title</Label>
+                    <Input type="text" name="title" value={this.state.newEvent.title} id="title" />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="description">Description</Label>
+                    <Input type="textarea" name="description" value={this.state.newEvent.description} id="description" />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="start-time">Start time</Label><br />
+                    <DatePicker
+                        name="start-time"
+                        selected={moment(this.state.newEvent.startTime).toDate()}
+                        onChange={date => this.setState({ newEvent: { startTime: date.toISOString() } })}
+                        showTimeSelect
+                        style={{ width: "100%" }}
+                        dateFormat="MMMM d, yyyy h:mm aa" />
+                </FormGroup>
+              </Form>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={this.toggle}>Save</Button>{' '}
+              <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
         </div>
     );
   }
